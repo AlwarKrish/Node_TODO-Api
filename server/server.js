51334ee8fd4762d257fbf8d47dd3bad3b5478e3f
Alwar <alwarg1998@gmail.com>
2017-11-29 13:49:07 +0530

    Add Get/todos route

--- server/server.js
+++ server/server.js
@@ -1,25 +1,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {user} = require('./models/users');
+var {User} = require('./models/users');
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res) => {
-  var todo = new Todo ({
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
     text: req.body.text
   });
-  //console.log(req.body);
+
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
-      res.status(400).send(e);
+    res.status(400).send(e);
   });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(3000, () => {
@@ -27,3 +34,50 @@ app.listen(3000, () => {
 });
 
 module.exports = {app};
+
+
+
+
+
+
+
+
+
+
+// var express = require('express');
+// var bodyParser = require('body-parser');
+//
+//
+// var {mongoose} = require('./db/mongoose');
+// var {Todo} = require('./models/todo');
+// var {user} = require('./models/users');
+//
+// var app = express();
+//
+// app.use(bodyParser.json());
+//
+// app.post('/todos',(req,res) => {
+//   var todo = new Todo ({
+//     text: req.body.text
+//   });
+//   //console.log(req.body);
+//   todo.save().then((doc) => {
+//     res.send(doc);
+//   }, (e) => {
+//       res.status(400).send(e);
+//   });
+// });
+//
+// app.get('/todos' ,(req,res) => {
+//   Todo.find().then(() => {
+//       res.send({todos});
+//   }, (e) => {
+//         res.status(400).send(e);
+//   });
+// });
+//
+// app.listen(3000, () => {
+//   console.log('Started on port 3000');
+// });
+//
+// module.exports = {app};
